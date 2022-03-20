09cf4f80735aa74c3f2d956fea870e09e5d03d5d
Owen Welsh <100965418+Solaniine@users.noreply.github.com>
2022-03-20 14:59:10 +0000

    Add files via upload

---
+++ Analyse.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    public class Analyse
+    {
+
+        //Handles the analysis of text
+
+        //Method: analyseText
+        //Arguments: string
+        //Returns: list of integers
+        //Calculates and returns an analysis of the text
+;
+
+        public List<int> analyseText(string input)
+        {
+
+
+            int i = 0;
+            //List of integers to hold the first five measurements:
+            //1. Number of sentences
+            int numberofsentences = 0;
+            for (i = 0; i < FileTextInput.Length; i++)
+            {
+
+                if (FileTextInput.Contains("."))
+                {
+                    numberofsentences++;
+                }
+            }
+            //2. Number of vowels
+            int numberofvowels = 0;
+            for (i = 0; i < FileTextInput.Length; i++)
+            {
+                
+                if (FileTextInput.Contains("a") || FileTextInput.Contains("e") || FileTextInput.Contains("i") || FileTextInput.Contains("o") || FileTextInput.Contains("u"))
+                {
+                    numberofvowels++;
+                }
+            }
+            //3. Number of consonants
+            int numberofcons = 0;
+            for(i =0; i <FileTextInput.Length; i++)
+            {
+                numberofcons =  FileTextInput.Length - numberofvowels;
+            }
+            //4. Number of upper case letters
+            int numberofupper = 0;
+            for(i = 0; i < FileTextInput.Length; i++)
+            {
+                if(FileTextInput.IsUpper(FileTextInput[i]))
+                {
+                    numberofupper++;
+                }
+            }
+            //5. Number of lower case letters
+            int numberoflower = 0;
+            for (i = 0; i < FileTextInput.Length; i++)
+            {
+                if (FileTextInput.IsLower(FileTextInput[i]))
+                {
+                    numberoflower++;
+                }
+            }
+            List<int> values = new List<int>();
+            //Initialise all the values in the list to '0'
+            for(int j = 0; j<5; j++)
+            {
+                values.Add(0);
+                Console.WriteLine("amount of sentences", numberofsentences);
+            }
+
+
+            return values;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//Base code project for CMP1903M Assessment 1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace CMP1903M_Assessment_1_Base_Code
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Local list of integers to hold the first five measurements of the text
+            List<int> parameters = new List<int>();
+            
+
+            //Create 'Input' object
+            //Get either manually entered text, or text from a file
+
+
+                Input text = new();
+                string text_file = text.FileTextInput();
+
+
+
+
+            //Create an 'Analyse' object
+                Analyse analysee = new Analyse();
+
+            //Receive a list of integers back
+
+
+            //Report the results of the analysis
+
+
+            //TO ADD: Get the frequency of individual letters?
+
+
+        }
+        
+        
+    
+    }
+}
